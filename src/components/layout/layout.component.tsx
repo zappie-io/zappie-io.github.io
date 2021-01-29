708d180d0dbebd76cb6902e1c9e1581dec92ac00
Maciej Olejnik <maciekolej@gmail.com>
2021-01-30 00:06:06 +0100

    Remove smooth scrolling on mobile

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,20 +1,17 @@
-import React, {
-  useState,
-  useCallback,
-  useRef,
-  useEffect,
-  useLayoutEffect,
-} from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { isBrowser } from "react-device-detect";
+
 import * as styles from "./layout.module.scss";
 
 const DAMPER = 0.09;
 
-const Layout = ({ children }) => {
+const BrowserLayout = ({ children }) => {
   const wrapperRef = useRef<HTMLDivElement>();
+
   const animationRef = useRef<number>();
   const [offset, setOffset] = useState(0.0);
 
-  const animate = (time) => {
+  const animate = () => {
     const scrollY = document.documentElement.scrollTop || 0.0;
     setOffset(offset + (scrollY - offset) * DAMPER);
     animationRef.current = requestAnimationFrame(animate);
@@ -38,12 +35,25 @@ const Layout = ({ children }) => {
       ref={wrapperRef}
       style={{
         transform: `translate3d(0, ${-offset.toFixed(2)}px, 0)`,
+        position: "fixed",
       }}
       className={styles.layout}
     >
       {children}
     </div>
   );
+};
+
+const MobileLayout = ({ children }) => {
+  return <div className={styles.layout}>{children}</div>;
+};
+
+const Layout = (props) => {
+  if (isBrowser) {
+    return <BrowserLayout {...props} />;
+  } else {
+    return <MobileLayout {...props} />;
+  }
 };
 
 export default Layout;
