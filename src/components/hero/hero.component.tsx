e2b2cd3c5573b373c83e73b1d703c7c0116ba0b5
Maciej Olejnik <maciekolej@gmail.com>
2021-02-07 16:22:27 +0100

    Add hero animation

--- src/components/hero/hero.component.tsx
+++ src/components/hero/hero.component.tsx
@@ -2,8 +2,45 @@ import * as React from "react";
 import { Link as GLink } from "gatsby";
 import * as styles from "./hero.module.scss";
 import heroImage from "src/images/hero.svg";
+import { useLayoutEffect, useRef, useState } from "react";
 
 const Hero = () => {
+  const phone = useRef(null);
+  const [offsetPhone, setOffsetPhone] = useState([0, 0]);
+
+  const darkDecor = useRef(null);
+  const [offsetDarkDecor, setOffsetDarkDecor] = useState([0, 0]);
+
+  const blured = useRef(null);
+  const [offsetBlured, setOffsetBlured] = useState([0, 0]);
+
+  const appleIcon = useRef(null);
+  const [offsetAppleIcon, setOffsetAppleIcon] = useState([0, 0]);
+
+  const androidIcon = useRef(null);
+  const [offsetAndroidIcon, setOffsetAndroidIcon] = useState([0, 0]);
+
+  const slide = (event, ratio, ref) => {
+    const { left, right } = ref.current.getBoundingClientRect();
+    return [(left - event.pageX) / ratio, (right - event.pageY) / ratio];
+  };
+
+  const onMove = (event: MouseEvent) => {
+    setOffsetDarkDecor(slide(event, 100, phone));
+    setOffsetPhone(slide(event, 60, phone));
+    setOffsetBlured(slide(event, 40, blured));
+    setOffsetAppleIcon(slide(event, 36, appleIcon));
+    setOffsetAndroidIcon(slide(event, 20, androidIcon));
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("mousemove", onMove);
+
+    return () => {
+      window.removeEventListener("mousemove", onMove);
+    };
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -26,31 +63,66 @@ const Hero = () => {
           </GLink>
         </div>
         <div className={styles.canvas}>
-          <div className={styles.appleIcon}></div>
-          <div className={styles.androidIcon}></div>
-          <div className={styles.phone}></div>
-          <div className={styles.circle}></div>
-          <div className={styles.darkDecor}></div>
-          <div className={styles.darkDecorMask}></div>
-          <div className={styles.blured}>
+          <div
+            ref={appleIcon}
+            style={{
+              transform: `translate3d(${offsetAppleIcon[0]}px, ${offsetAppleIcon[1]}px, 0)`,
+            }}
+            className={styles.appleIcon}
+          />
+          <div
+            ref={androidIcon}
+            style={{
+              transform: `translate3d(${offsetAndroidIcon[0]}px, ${offsetAndroidIcon[1]}px, 0)`,
+            }}
+            className={styles.androidIcon}
+          />
+          <div
+            ref={phone}
+            className={styles.phone}
+            style={{
+              transform: `translate3d(${offsetPhone[0]}px, ${offsetPhone[1]}px, 0)`,
+            }}
+          />
+          <div
+            ref={darkDecor}
+            style={{
+              transform: `translate3d(${offsetDarkDecor[0]}px, ${offsetDarkDecor[1]}px, 0)`,
+            }}
+            className={styles.darkDecor}
+          />
+          <div
+            style={{
+              transform: `translate3d(${offsetPhone[0]}px, ${offsetPhone[1]}px, 0)`,
+            }}
+            className={styles.darkDecorMask}
+          >
+            <div className={styles.circle} />
+          </div>
+          <div
+            ref={blured}
+            style={{
+              transform: `translate3d(${offsetBlured[0]}px, ${offsetBlured[1]}px, 0)`,
+            }}
+            className={styles.blured}
+          >
             <div className={styles.figmaWrapper}>
-              <div className={styles.figmaIcon}></div>
+              <div className={styles.figmaIcon} />
               <div className={styles.textWrapper}>
-                <div className={styles.textWrapperTitle}></div>
-                <div className={styles.textWrapperDesc}></div>
+                <div className={styles.textWrapperTitle} />
+                <div className={styles.textWrapperDesc} />
               </div>
             </div>
 
             <div className={styles.reactWrapper}>
-              <div className={styles.figmaIcon}></div>
+              <div className={styles.figmaIcon} />
               <div className={styles.textWrapper}>
-                <div className={styles.textWrapperTitle}></div>
-                <div className={styles.textWrapperDesc}></div>
+                <div className={styles.textWrapperTitle} />
+                <div className={styles.textWrapperDesc} />
               </div>
             </div>
           </div>
         </div>
-        {/* <img className={styles.heroImage} src={heroImage} alt="Zappie hero" /> */}
       </div>
     </div>
   );
